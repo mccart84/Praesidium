7ba8a02e62fd1e617a9b036373694ef8f801806b
Ryan McCarty <ryan.mccarty@northpointeinc.com>
2017-02-07 10:40:13 -0500

    checking for sections

--- Praesidium/Praesidium/DAL/NavigationItems.cs
+++ Praesidium/Praesidium/DAL/NavigationItems.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Praesidium.Data_Models;
 
 namespace Praesidium.DAL
 {
@@ -11,7 +12,7 @@ namespace Praesidium.DAL
     {
         public IQueryable<NavItem> GetActiveNavigationItems()
         {
-            var navItems = _context.ShSyNavigationItems.Where(x => x.IsActive && x.ParentId == null).Select(x => new NavItem
+            var navItems = _context.ShSyNavigationItems.Where(x => x.IsActive && x.ParentId == null && x.ShSySection.IsActive == true).Select(x => new NavItem
             {
                 RecId = x.RecId,
                 Controller = x.Controller,
