371f0519d8489a31702f55caa3951fecfe0f27a3
Tyler <Tyler@DESKTOP-89C4CTR>
2017-02-17 20:18:04 -0500

    Login Page

--- Praesidium/Praesidium/Controllers/HomeController.cs
+++ Praesidium/Praesidium/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace Praesidium.Controllers
     {
         public ActionResult Index()
         {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
             return View();
         }
 
