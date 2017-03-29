935cdcfc6770f6f60dacc3785717f6f8de06d224
Russ McGinley <russmcginley@hotmail.com>
2017-03-28 21:53:14 -0400

    Added file views to pages

--- Praesidium/Praesidium/Controllers/traffickingController.cs
+++ Praesidium/Praesidium/Controllers/traffickingController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using Praesidium.Data_Models.Admin;
+using Praesidium.DAL;
 
 namespace Praesidium.Controllers
 {
@@ -16,37 +20,62 @@ namespace Praesidium.Controllers
 
         public ActionResult Resources()
         {
-            return View();
+            return View(GetFilesByCategory(11));
         }
 
         public ActionResult Statistics()
         {
-            return View();
+            return View(GetFilesByCategory(10));
         }
 
         public ActionResult Adults()
         {
-            return View();
+            
+            return View(GetFilesByCategory(6));
         }
 
         public ActionResult Teachers()
         {
-            return View();
+            return View(GetFilesByCategory(9));
         }
 
         public ActionResult Teen()
         {
-            return View();
+            return View(GetFilesByCategory(7));
         }
 
         public ActionResult Youth()
         {
-            return View();
+            return View(GetFilesByCategory(8));
         }
 
         public ActionResult WarningSigns()
         {
             return View();
+        }
+
+        public List<FileView> GetFilesByCategory(int catId)
+        {
+            AdminEntities db = new AdminEntities();
+            var files = new List<FileView>();
+            try
+            {
+                var catfiles = db.ShFileKeywords.Where(m => m.Keyword == catId.ToString());
+
+                foreach (var t in catfiles)
+                {
+                    FileView file = db.FileViews.First(u => u.RecId == t.FkShFile);
+                    files.Add(file);
+                }
+
+                return files;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
         }
     }
 }
