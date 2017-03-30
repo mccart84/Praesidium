7f785a8ad08433ebca2225489878590ee1488e7e
Russ McGinley <russ.mcginley@gmail.com>
2017-03-30 13:07:00 -0400

    File Views

---
+++ Praesidium/Praesidium/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Praesidium.Data_Models.Admin;
+
+namespace Praesidium.Controllers
+{
+    public class FileController : Controller
+    {
+        private AdminEntities db = new AdminEntities();
+        // GET: Download
+        public FileResult Download(int id)
+        {
+            ShFile file = db.ShFiles.Find(id);
+            if (file != null)
+            {
+                file.DownloadCount++;
+                db.SaveChanges();
+                var newfile = new FileContentResult(file.FileStore, file.ContentType);
+                newfile.FileDownloadName = file.FileName;
+                return newfile;
+            }
+
+            else
+            {
+                throw new HttpException(404, "File Not Found");
+            }
+
+        }
+        // GET: File
+        public ActionResult Show(int id)
+        {
+            var file = db.FileViews.Find(id);
+            if (file == null)
+                throw new HttpException(404, "Invalid File Specified");
+            else
+                return View(file);
+        }
+    }
+}
