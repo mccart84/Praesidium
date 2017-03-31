d6cd9d98bdad15fe340a955f10b794c5dcc7ffc9
Russ McGinley <russmcginley@hotmail.com>
2017-03-30 21:58:51 -0400

    Fixed some minor bugs, and added file viewing to the buttons

--- Praesidium/Praesidium/Controllers/FileController.cs
+++ Praesidium/Praesidium/Controllers/FileController.cs
@@ -15,9 +15,6 @@ namespace Praesidium.Controllers
         {
             try
             {
-                if (!id.HasValue)
-                    throw new HttpException(404, "No File ID Specified");
-
                 ShFile file = db.ShFiles.Find(id);
                 if (file != null)
                 {
@@ -30,7 +27,7 @@ namespace Praesidium.Controllers
 
                 else
                 {
-                    throw new HttpException(404, "File Not Found");
+                    return null;
                 }
 
             }
@@ -46,12 +43,23 @@ namespace Praesidium.Controllers
             try
             {
                 if (!id.HasValue)
-                { return View("Index","Home"); }
+                { return RedirectToAction("Index", "Home"); }
                 var file = db.FileViews.Find(id);
                 if (file == null)
-                { return View("Index", "Home"); }
+                {
+                    return RedirectToAction("Index", "Home");
+                }
                 else
+                {
+                    var fileview = db.ShFiles.Find(id);
+                    if (fileview != null)
+                    {
+                        fileview.ViewCount++;
+                        db.SaveChanges();
+                    }
                     return View(file);
+                }
+
             }
             catch (Exception e)
             {
